649d4d610ba69ec2764102a3750acedcefaea3fb
Jacob Loehr <jlloehr@email.wm.edu>
2020-05-17 20:51:30 -0400

    First commit, writing to SQLite table

--- src/com/codingchallenge/SQLiteFromCSV.java
+++ src/com/codingchallenge/SQLiteFromCSV.java
@@ -3,13 +3,13 @@ package com.codingchallenge;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.sql.*; //just import what i need
 
 public class SQLiteFromCSV {
 	private static String JDBC_CONNECTION_URL = 
 			"jdbc:sqlite:/Users/jacobloehr/sqlite/challenge.db";
-		//jdbc:sqlite:C:/sqlite/db/chinook.db
 
 	
 	public static void main(String[] args) {
@@ -25,10 +25,15 @@ public class SQLiteFromCSV {
 		int totalRows = 0;
 		int validRows = 0;
 		int invalidRows = 0;
-		String a,b,c,d,e_,f,g,h,i,j = "";
+		String A,B,C,D,E,F,G,H,I,J = "";
 		Connection connection = null;
+		
+		//setup SQLite connection
 		try {
 			connection = DriverManager.getConnection(JDBC_CONNECTION_URL);
+			//if rerunning program, drop and create new table
+			connection.createStatement().execute("drop table customer");
+			connection.createStatement().execute("create table customer(A,B,C,D,E,F,G,H,I,J)");
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -41,33 +46,38 @@ public class SQLiteFromCSV {
                 // use comma as separator
             	
                 row = line.split(cvsSplitBy);
-                System.out.println(row.length);
                 //since there will be a comma in row E, it will be split into 11 entries instead of 10  
                 if(row.length > 0 && row.length == validColumnNumber +1 ) {
                 	//System.out.println(row[0]);
                 	
                 	validRows++;
                 	try {
-                		a = row[0];
-                		System.out.println(a);
-                		b = row[1];
-                		System.out.println(b);
-                		c = row[2];
-                		System.out.println(c);
-                		d = row[3];
-                		System.out.println(d);
-                		e_ = row[4] + "," + row[5];
-                		System.out.println(e_);
-                		f = row[6];
-                		System.out.println(f);
-                		g = row[7];
-                		h = row[8];
-                		i = row[9];
-                		j = row[10];
-                		//PreparedStatement statement = connection.prepareStatement("insert into customer(A,B,C,D,E,F,G,H,I,J) values (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])");
-                		//PreparedStatement statement = connection.prepareStatement("insert into customer(A,B,C,D,E,F,G,H,I,J) values ('q','q','q','q','q','q','q','q','q','q')");
-                		PreparedStatement statement = connection.prepareStatement("insert into customer(A,B,C,D,E,F,G,H,I,J) values (a,b,c,d,e_,f,g,h,i,j)");
+                		A = row[0];                		
+                		B = row[1];                		
+                		C = row[2];                		
+                		D = row[3];      
+                		//joining indexes 4 and 5 because they were previously separated by a comma  
+                		E = row[4] + "," + row[5];               		
+                		F = row[6];
+                		G = row[7];
+                		H = row[8];
+                		I = row[9];
+                		J = row[10];
+                		
+                		PreparedStatement statement = connection.prepareStatement("insert into customer(A,B,C,D,E,F,G,H,I,J) values (?,?,?,?,?,?,?,?,?,?)");
+                		                		
+                		statement.setString(1, A);
+                		statement.setString(2, B);
+                		statement.setString(3, C);
+                		statement.setString(4, D);
+                		statement.setString(5, E);
+                		statement.setString(6, F);
+                		statement.setString(7, G);
+                		statement.setString(8, H);
+                		statement.setString(9, I);
+                		statement.setString(10, J);
                 		statement.executeUpdate();
+                		
                 	}
                 	catch (SQLException e) {
             			e.printStackTrace();
@@ -77,6 +87,7 @@ public class SQLiteFromCSV {
                 else {
                 	invalidRows++;
                 	//write to csv file
+                	System.out.println(line);
                 }
 
             }
